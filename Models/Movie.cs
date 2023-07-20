fed0bec58f68b9bb6a29d4a5f30ab9c82cb5bf8b
Sarah Muzapar <wardina29sara@gmail.com>
2023-07-20 22:52:56 +0800

    update until part 6

---
+++ Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; } //primary key
+        public string? Title { get; set; } //? means property is nullable
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; } // user does not required to enter time information in the date field
+        public string? Genre { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+    }
+}
